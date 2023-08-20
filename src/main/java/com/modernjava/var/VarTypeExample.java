64a0b24abedfaed691c8a5d9a9fa3832211073ce
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-08-20 05:34:49 -0500

    completing var

---
+++ src/main/java/com/modernjava/var/VarTypeExample.java
@@ -0,0 +1,18 @@
+package com.modernjava.var;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class VarTypeExample {
+
+
+    public static void main(String[] args) {
+
+        List<String> names1 = List.of("adam", "dilip");
+    }
+
+}
+
