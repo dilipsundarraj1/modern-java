b9875ad4abf39058d6c13290f58153d23a1ee6cd
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-08-20 05:26:34 -0500

    added the intial code

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_20
+    targetCompatibility = JavaVersion.VERSION_20
+}
+
+//tasks.withType(JavaCompile).each {
+//    it.options.compilerArgs.add('--enable-preview')
+//}
+
+tasks.withType<JavaCompile> {
+    val compilerArgs = options.compilerArgs
+    compilerArgs.add("--enable-preview")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    jvmArgs(listOf("--enable-preview"))
+}
