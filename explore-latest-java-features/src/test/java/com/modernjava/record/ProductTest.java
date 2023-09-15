2897f0b5a05fd368be42cd795b132a9a59731aa3
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-09-15 14:26:41 -0500

    added the classes for record and recordtest

---
+++ explore-latest-java-features/src/test/java/com/modernjava/record/ProductTest.java
@@ -0,0 +1,61 @@
+package com.modernjava.record;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ProductTest {
+
+    @Test
+    void createProduct() {
+
+        var product = new Product("IPhone", new BigDecimal("999.99"),
+                "ELECTRONICS");
+        System.out.println("product = " + product);
+        assertEquals("IPhone", product.name());
+        assertEquals("ELECTRONICS", product.type());
+
+        // Explain about immutablility
+        /**
+         * There are no setter methods to access and modify the values of the instance variable.
+         */
+    }
+
+    @Test
+    void createProduct_customConstructor() {
+
+        var product = new Product("IPhone", new BigDecimal("999.99"));
+        System.out.println("product = " + product);
+        assertEquals("IPhone", product.name());
+        assertEquals("GENERAL", product.type());
+
+    }
+
+    @Test
+    void createProduct_error() {
+
+
+        var exception = assertThrows(IllegalArgumentException.class,
+                ()-> new Product("IPhone", new BigDecimal("-999.99"),
+                        "ELECTRONICS")                );
+
+        assertEquals("cost value is not valid!", exception.getMessage());
+    }
+
+
+    @Test
+    void createProduct_name_notValid() {
+
+
+        var exception = assertThrows(IllegalArgumentException.class,
+                ()-> new Product("", new BigDecimal("999.99"),
+                        "ELECTRONICS")                );
+
+        assertEquals("name value is not valid!", exception.getMessage());
+    }
+
+
+}
