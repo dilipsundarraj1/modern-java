78341a2d41cfe42a648bd18d9d65d6b1124442e5
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-09-15 14:25:45 -0500

    added the product test class

---
+++ explore-latest-java-features/src/test/java/com/modernjava/record/ProductTest.java
@@ -0,0 +1,14 @@
+package com.modernjava.record;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ProductTest {
+
+  
+
+}
