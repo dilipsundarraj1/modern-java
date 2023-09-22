298f39968ee1fd8c44f4ac471ab83a3fc9b0070f
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-09-22 05:01:23 -0500

    code for record patterns is set

--- explore-latest-java-features/src/main/java/com/modernjava/patternmatching/recordMatch/AnimalService.java
+++ explore-latest-java-features/src/main/java/com/modernjava/patternmatching/recordMatch/AnimalService.java
@@ -13,7 +13,7 @@ public class AnimalService {
 
     public String retrieveNameV2(Animal animal){
         return switch (animal){
-            case null -> "";  // This handles the null pointer exception.
+
 //            case Car(String name,
 //                     String color) -> name;
             //type inference can also be used in the binding variable.
@@ -21,6 +21,9 @@ public class AnimalService {
                      var color) -> name;
             case Dog(String name,
                      String color) -> name;
+            // The null case is must and it handles the null pointer exception.
+            // It does not matter where this case statement is kept.
+            case null -> "";
         };
 
     }
