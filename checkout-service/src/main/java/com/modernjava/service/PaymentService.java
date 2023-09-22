13090fe4268aef11d7de57e7c345c81476f22315
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-09-22 09:53:25 -0500

    removed unwanted code

--- checkout-service/src/main/java/com/modernjava/service/PaymentService.java
+++ checkout-service/src/main/java/com/modernjava/service/PaymentService.java
@@ -5,19 +5,6 @@ import com.modernjava.payment.*;
 
 public class PaymentService {
 
-
-    public PaymentResponse makePayment(OrderDetails orderDetails, PaymentGateway paymentGateway) {
-
-        if(paymentGateway instanceof DebitCardPayment debitCardPayment){
-            debitCardPayment.makePayment(orderDetails.card(), orderDetails.finalAmount());
-            return PaymentResponse.SUCCESS;
-        }else if(paymentGateway instanceof  CreditCardPayment creditCard){
-            creditCard.makePayment(orderDetails.card(), orderDetails.finalAmount());
-            return PaymentResponse.FAILURE;
-        }
-        return null; // Optional is another way of treating a return value has an actual value or null.
-    }
-
     public PaymentResponse makePaymentv2(OrderDetails orderDetails) {
 
         var paymentGateway = PaymentFactory.paymentGateway(orderDetails.card().cardType());
