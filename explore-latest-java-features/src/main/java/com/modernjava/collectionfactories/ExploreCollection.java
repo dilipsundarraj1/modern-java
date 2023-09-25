a992fe789d09b1b59b0638d43966b8f9cabd3687
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-09-25 12:48:09 -0500

    added the readme content and the collection factories

---
+++ explore-latest-java-features/src/main/java/com/modernjava/collectionfactories/ExploreCollection.java
@@ -0,0 +1,13 @@
+package com.modernjava.collectionfactories;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Map.entry;
+
+public class ExploreCollection {
+
+    public static void main(String[] args) {
+    }
+}
