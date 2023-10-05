397327328f2cceb97a857bccc45db17c4c48ec0e
dilip sundarraj <dilip.bandlasundarraj@gmail.com>
2023-10-05 07:56:22 -0500

    added the tests for the httpclient

--- explore-latest-java-features/src/main/java/com/modernjava/http/MoviesClient.java
+++ explore-latest-java-features/src/main/java/com/modernjava/http/MoviesClient.java
@@ -1,5 +1,6 @@
 package com.modernjava.http;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -12,6 +13,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static java.net.http.HttpRequest.newBuilder;
 
@@ -21,19 +23,38 @@ public class MoviesClient {
             .connectTimeout(Duration.ofSeconds(2))
             .build();
 
-    public  static String ALL_MOVIES_URL = "http://127.0.0.1:8000/explore-latest-java-features/src/main/resources/movies.json";
-    public  static String MOVIE_BY_ID_URL = "http://127.0.0.1:8000/explore-latest-java-features/src/main/resources/movie_by_id.json";
+    public static String ALL_MOVIES_URL = "http://127.0.0.1:8000/explore-latest-java-features/src/main/resources/movies.json";
+    public static String MOVIE_BY_ID_URL = "http://127.0.0.1:8000/explore-latest-java-features/src/main/resources/movie_by_id.json";
 
-        private final ObjectMapper objectMapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
-    public List<Movie> getAllMovies() {
+    public HttpResponse<String> getAllMovies() {
 
         try {
 
-            var request= requestBuilder(ALL_MOVIES_URL);
+            var request = requestBuilder(ALL_MOVIES_URL);
             HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Status code: " + response.statusCode());
+            System.out.println("Headers: " + response.headers());
+            System.out.println("Body: " + response.body());
+            return response;
+        } catch (IOException | InterruptedException e) {
+            System.err.println(e);
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Movie> getAllMoviesV2() {
+
+        try {
+
+            var request = requestBuilder(ALL_MOVIES_URL);
+            var response =
                     client.send(request, HttpResponse.BodyHandlers.ofString());
 
             System.out.println("Status code: " + response.statusCode());
@@ -42,6 +63,33 @@ public class MoviesClient {
             return objectMapper.readValue(response.body(), new TypeReference<>() {
             });
         } catch (IOException | InterruptedException e) {
+            System.err.println(e);
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public CompletableFuture<List<Movie>> getAllMoviesV2Async() {
+
+        try {
+            var request = requestBuilder(ALL_MOVIES_URL);
+            var response =
+                    client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+            return response
+                    .thenApply(httpResponse -> {
+                        System.out.println("Status code: " + httpResponse.statusCode());
+                        System.out.println("Headers: " + httpResponse.headers());
+                        try {
+                            return objectMapper.readValue(httpResponse.body(), new TypeReference<>() {
+                            });
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+
+        } catch (Exception e) {
+            System.err.println(e);
             throw new RuntimeException(e);
         }
 
@@ -51,15 +99,42 @@ public class MoviesClient {
 
         try {
 
-            var request= requestBuilder(MOVIE_BY_ID_URL);
+            var request = requestBuilder(MOVIE_BY_ID_URL);
             HttpResponse<String> response =
                     client.send(request, HttpResponse.BodyHandlers.ofString());
 
             System.out.println("Status code: " + response.statusCode());
             System.out.println("Headers: " + response.headers());
 
+
             return objectMapper.readValue(response.body(), Movie.class);
         } catch (IOException | InterruptedException e) {
+            System.err.println(e);
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public CompletableFuture<Movie> getMovieByIdAsync() {
+
+        try {
+
+            var request = requestBuilder(MOVIE_BY_ID_URL);
+            var response =
+                    client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+            return response
+                    .thenApply(httpResponse -> {
+                        System.out.println("Status code: " + httpResponse.statusCode());
+                        System.out.println("Headers: " + httpResponse.headers());
+                        try {
+                            return objectMapper.readValue(httpResponse.body(),Movie.class);
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        } catch (Exception e) {
+            System.err.println(e);
             throw new RuntimeException(e);
         }
 
--- explore-latest-java-features/src/test/java/com/modernjava/http/MoviesClientTest.java
+++ explore-latest-java-features/src/test/java/com/modernjava/http/MoviesClientTest.java
@@ -4,22 +4,43 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@Disabled
+//@Disabled
 public class MoviesClientTest {
 
     MoviesClient moviesClient = new MoviesClient();
 
     @Test
-
     void getAllMovies() {
-        var moviesList = moviesClient.getAllMovies();
+        var movieResponse = moviesClient.getAllMovies();
+        assertEquals(200, movieResponse.statusCode());
+        assertNotNull(movieResponse.body());
+
+    }
+
+
+    @Test
+    void getAllMoviesV2() {
+        var moviesList = moviesClient.getAllMoviesV2();
+        assert moviesList.size()==10;
+    }
+
+    @Test
+    void getAllMoviesV2Async() {
+        var moviesList = moviesClient.getAllMoviesV2Async().join();
         assert moviesList.size()==10;
     }
 
     @Test
     void getMovieById() {
         var movie = moviesClient.getMovieById();
+        assertEquals("Batman Begins", movie.name());
+    }
+
+    @Test
+    void getMovieByIdAsync() {
+        var movie = moviesClient.getMovieByIdAsync().join();
         assertEquals("Batman Begins", movie.name());
     }
 }
