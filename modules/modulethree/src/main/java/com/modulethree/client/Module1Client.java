c0d0e51bed3fe5c14cc79be370a25d50075b6d98
dilip sundarraj <dilip.bandlasundarraj@gmail.com>
2023-10-06 10:44:46 -0500

    added the necessary cpde

---
+++ modules/modulethree/src/main/java/com/modulethree/client/Module1Client.java
@@ -0,0 +1,15 @@
+package com.modulethree.client;
+
+
+public class Module1Client {
+
+//    private final Module1Service module1Service;
+//
+//    public Module1Client(Module1Service module1Service) {
+//        this.module1Service = module1Service;
+//    }
+//
+//    public Module1DTO retrieveData(){
+//        return module1Service.retrieveData();
+//    }
+}
---
+++ modules/modulethree/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+//module modulethree {
+//    requires moduletwo;
+//
+//}
