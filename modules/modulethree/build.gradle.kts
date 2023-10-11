4436feed6d92df908921f732c6a895ed2c6ea272
dilip sundarraj <dilip.bandlasundarraj@gmail.com>
2023-10-11 05:52:38 -0500

    updated readme

--- modules/modulethree/build.gradle.kts
+++ modules/modulethree/build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
-//    implementation(project(":modules:moduletwo"))
-//    implementation(project(":modules:moduleone"))
+    implementation(project(":modules:moduletwo"))
+    implementation(project(":modules:moduleone"))
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
